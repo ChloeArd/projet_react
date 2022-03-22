24887bcb82b32343ebba0b07fe3b0fec2a11e9da
ChloeArd <chloe.ardoise@gmail.com>
2022-03-22 09:34:28 +0100

    Projet react

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,10 +8,21 @@ export const Cart = function ({products}) {
         console.log('Le bouton vider le panier a été cliqué !');
     }
 
+    const getCartItem = () => {
+        let jsxCartItem = [];
+        for (let i = 0; i < products.length; i++) {
+            // Displays products that are added to cart
+            if (products[i].cart > 0) {
+                jsxCartItem.push(<CartItem key={products[i].id} product={products[i]} />)
+            }
+        }
+        return jsxCartItem;
+    }
+
     return (
         <div className="Cart">
             <h1 className="title">Vos articles</h1>
-            {products.map(product => <CartItem key={product.id} product={product} />)}
+            {getCartItem()}
             <button className="empty" onClick={handleCartEmptyButtonClick}>Vider le panier</button>
         </div>
     );
